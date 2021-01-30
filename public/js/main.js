7fbf346053a50ae81b1a42f8cbb9f05ecdb45c6f
Abdellah <mounajaikabdellah@gmail.com>
2021-01-31 00:41:25 +0100

    exo_fini

---
+++ public/js/main.js
@@ -0,0 +1,41 @@
+// exo1
+
+// let prenom = prompt("prenom?");
+// console.log(prenom.substr(0, 1));
+// alert(prenom.substr(0, 1))
+
+// exo2
+
+// let prenom = prompt("prenom?");
+// console.log(prenom.substr(-1, 1));
+// alert(prenom.substr(-1, 1))
+
+// exo3
+
+// let prenom = prompt("prenom?");
+// console.log(prenom.substr(1));
+// alert(prenom.substr(1))
+
+// exo4
+
+// let prenom = prompt("prenom?");
+// console.log(prenom.charAt(0).toLowerCase() + prenom.charAt(1).toUpperCase() + prenom.slice(2));
+// alert(prenom.charAt(0).toLowerCase() + prenom.charAt(1).toUpperCase() + prenom.slice(2));
+
+// exo5
+
+// let prenom = prompt("prenom?").toLocaleLowerCase();
+// let lettre = prompt(`Quelle lettre voulez-vous mettre en majuscule dans '${prenom}'`).toLowerCase();
+// alert(prenom.replace(lettre, lettre.toUpperCase()));
+
+// exo6
+
+// let prenom = prompt("prenom?");
+// console.log(prenom.charAt(0).toUpperCase() + prenom.slice(1));
+// alert(prenom.charAt(0).toUpperCase() + prenom.slice(1));
+
+// exo7
+
+// let prenom = prompt("prenom?");
+// let nbal = Math.round(Math.random()*prenom.length);
+// alert(prenom.replace(prenom.charAt(nbal), ""));
